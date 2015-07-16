08729c6758d2ed68a3014953695d3bfa949089b8
Giovanni Gonzaga <giovanni@buildo.io>
2015-07-16 16:43:21 +0200

    fix test

--- test/tests/index.js
+++ test/tests/index.js
@@ -1,12 +1,41 @@
-import fs from 'fs';
-import path from 'path';
 import sassVariables from '../../src';
 import expect from 'expect';
 
 describe('sass-variables loader', () => {
   it('should work', () => {
 
-    const sass = fs.readFileSync(path.resolve(__dirname, '../variables.scss'), 'utf8');
+    const sass = `
+  $silver: #bdc2ca;
+  $charcoal: #2c3a40;
+  $pearl: #e6e6e6;
+  $gray: #dfdfdf;
+  $aquamarine: #45a7b9;
+  $shakespeare: #4dbfd3;
+  $green: #299f51;
+  $cello: #23448a;
+  $lynch: #628292;
+  $fiord: #384952;
+  $snow: #f5f7fa;
+  $ice: #edf8ff;
+  $perano: #bfe0f6;
+  $tangerine: #ff8d0f;
+  $tangerine60: rgba(255, 141, 15, 0.6);
+  $lemon: #ffea0f;
+  $lemon60: rgba(255, 233, 15, 0.6);
+  $vermillion: #fb1919;
+  $vermillion60: rgba(251, 25, 25, 0.6);
+  $cobalt: #0044a2;
+  $cobalt60: rgba(0, 68, 162, 0.6);
+
+  $border-color: $pearl;
+
+  $padding-small: 7px;
+  $padding-medium: 10px;
+  $padding-large: 15px;
+
+  $border-radius: 5px;
+  $border-radius-small: 2px;
+`;
 
     const parsed = JSON.parse(
       sassVariables(sass).replace(/module\.exports = ([^;]+);/, (_, m) => m)
